84e774b3a8c40dacd50dc108d0bfab3aa15b3230
Eden Marco <emarco177@gmail.com>
2023-02-25 16:38:14 +0200

    implemented all YAML DB endpoints

--- api/yaml_crud.py
+++ api/yaml_crud.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 import yaml
@@ -31,19 +32,60 @@ async def create_yaml(request: Request):
 
 @router.get("/{yaml_doc_id}")
 def read_yaml(yaml_doc_id: str):
-    return {"detail": "Hello World"}
+    dict_with_id = {"id": yaml_doc_id}
+    with open(f"yaml_db/{yaml_doc_id}.yaml", "r") as f:
+        yaml_data = yaml.load(f, Loader=yaml.loader.Loader)
+        if type(yaml_data) == str:
+            returned_doc = {**dict_with_id, "data": yaml_data}
+        else:
+            returned_doc = {**dict_with_id, **yaml_data}
+
+    return returned_doc
 
 
 @router.put("/{yaml_doc_id}")
 async def update_yaml(yaml_doc_id: str, request: Request):
-    return {"detail": "Hello World"}
+    raw_body = await request.body()
+    try:
+        loaded_yaml = yaml.load(raw_body, Loader=yaml.Loader)
+    except yaml.YAMLError as e:
+        raise HTTPException(status_code=422, detail="Invalid YAML")
+
+    file_path = f"{os.getcwd()}/yaml_db/{yaml_doc_id}.yaml"
+    if not os.path.isfile(file_path):
+        raise HTTPException(status_code=404, detail=f"YAML doc {yaml_doc_id} not found")
+
+    os.remove(file_path)
+
+    with open(f"yaml_db/{yaml_doc_id}.yaml", "w") as f:
+        yaml.dump(loaded_yaml, f, sort_keys=False, default_flow_style=False)
+
+    return f"update yaml doc with id {yaml_doc_id}"
 
 
 @router.delete("/{yaml_doc_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_yaml(yaml_doc_id: str):
-    return {"detail": "Hello World"}
+    file_path = f"{os.getcwd()}/yaml_db/{yaml_doc_id}.yaml"
+    if not os.path.isfile(file_path):
+        raise HTTPException(status_code=404, detail=f"YAML doc {yaml_doc_id} not found")
+
+    os.remove(file_path)
 
 
 @router.get("/")
 def read_yaml_list():
-    return {"detail": "Hello World"}
+    logger.info(f"getting all yaml docs from server...")
+
+    loaded_yamls = []
+    for root, _, files in os.walk("./yaml_db"):
+        for name in files:
+            file_path = os.path.join(root, name)
+            logger.info(f"{file_path}")
+            with open(file_path, "r") as f:
+                yaml_data = yaml.load(f, Loader=yaml.loader.Loader)
+                if yaml_data:
+                    yaml_data["id"] = name.split(".")[0]
+
+            loaded_yamls.append(yaml_data)
+
+    return {"data": loaded_yamls}
